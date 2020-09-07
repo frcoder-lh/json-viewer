1b2e198dc8185ed663a1ae677d7d6c67d7b073af
李辉 <frcoder.lh@hotmail.com>
2020-09-07 18:11:12 +0800

    增加模糊搜索功能

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var setting = {
     encodeType: "utf-8", expandLevel: 0,
     view: {
         txtSelectedEnable: true, nameIsHTML: true, showTitle: false
+    },
+    callback: {
+        onRightClick: zTreeOnRightClick
     }
 };
 
@@ -22,7 +25,7 @@ $('#in').blur(function () {
 });
 
 
-$('#in').bind('input propertychange DOMNodeInserted', function () {
+$('#in').bind('input propertychange', function () {
     $(".tip").hide();
     $.fn.zTree.init($("#out"), setting, getOutJson());
     tree = $.fn.zTree.getZTreeObj("out");
@@ -124,22 +127,18 @@ function parseJson(inJson, outJson) {
     for (let key in inJson) {
         if (inJson instanceof Array) {
             if (inJson[key] instanceof Object) {
-                let item = {name: '<span class="name">[' + htmlEncode(key) + ']</span>', children: []};
+                let item = createNode('[' + key + ']', []);
                 parseJson(inJson[key], item);
                 outJson.children.push(item);
-            } else if (typeof (inJson[key]) === 'string') {
-                outJson.children.push({name: '<span class="name">' + htmlEncode(key) + '</span>: "' + htmlEncode(inJson[key]) + '"'});
             } else {
-                outJson.children.push({name: '<span class="name">' + htmlEncode(key) + '</span>: ' + htmlEncode(inJson[key])});
+                outJson.children.push(createNode(key, inJson[key]));
             }
         } else if (inJson[key] instanceof Object) {
-            let item = {name: '<span class="name">' + htmlEncode(key) + '</span>', children: []};
+            let item = createNode(key, []);
             parseJson(inJson[key], item);
             outJson.children.push(item);
-        } else if (typeof (inJson[key]) === 'string') {
-            outJson.children.push({name: '<span class="name">' + htmlEncode(key) + '</span>: "' + htmlEncode(inJson[key]) + '"'});
         } else {
-            outJson.children.push({name: '<span class="name">' + htmlEncode(key) + '</span>: ' + htmlEncode(inJson[key])});
+            outJson.children.push(createNode(key, inJson[key]));
         }
     }
 }
@@ -148,9 +147,20 @@ function getOutJson() {
     let inJson = getInJson();
     if (!inJson) return null;
 
-    let outJson = {name: "JSON", children: []};
+    let outJson = createNode("JSON", []);
     parseJson(inJson, outJson);
     return outJson;
+}
+
+function createNode(key, value, encodeFlag, quoteFlag) {
+    if (encodeFlag == true || encodeFlag == null) key = htmlEncode(key);
+    if (value instanceof Array) {
+        return {originalKey: key, originalValue: value, name: '<span class="name">' + key + '</span> ', children: value};
+    } else {
+        if (typeof value === 'string' && (quoteFlag == true || quoteFlag == null)) value = '"' + value + '"';
+        if (encodeFlag == true || encodeFlag == null) value = htmlEncode(value);
+        return {originalKey: key, originalValue: value, name: '<span class="name">' + key + '</span>: ' + value};
+    }
 }
 
 function htmlEncode(value) {
@@ -160,3 +170,134 @@ function htmlEncode(value) {
 function htmlDecode(value) {
     return $('<div/>').html(value).text();
 }
+
+(function fuzzySearch() {
+
+    function ztreeFilter(zTreeObj, _keywords) {
+        if (!_keywords) {
+            _keywords = "";
+        }
+
+        function filterFunc(node) {
+            if (node && node.oldname && node.oldname.length > 0) {
+                node.name = node.oldname;
+            }
+            zTreeObj.updateNode(node);
+            if (_keywords.length == 0) {
+                zTreeObj.showNode(node);
+                zTreeObj.expandNode(node, true);
+                return true;
+            }
+            if (node.name) {
+                let show = false;
+                let rexGlobal = new RegExp(_keywords, "gi");
+                let originalKey = node.originalKey.replace(rexGlobal, function (originalText) {
+                    show = true;
+                    return '<span class="searchTarget">' + originalText + '</span>';
+                });
+                let originalValue = node.originalValue;
+                if (!(node.originalValue instanceof Array)) {
+                    originalValue = node.originalValue.replace(rexGlobal, function (originalText) {
+                        show = true;
+                        return '<span class="searchTarget">' + originalText + '</span>';
+                    });
+                }
+                node.oldname = node.name;
+                if (show) {
+                    node.name = createNode(originalKey, originalValue, false, false).name;
+                    zTreeObj.updateNode(node);
+                    zTreeObj.showNode(node);
+                    return true;
+                }
+            }
+            zTreeObj.hideNode(node);
+            return false;
+        }
+
+        var nodesShow = zTreeObj.getNodesByFilter(filterFunc);
+        processShowNodes(nodesShow, _keywords);
+    }
+
+    function processShowNodes(nodesShow, _keywords) {
+        if (nodesShow && nodesShow.length > 0) {
+            if (_keywords.length > 0) {
+                $.each(nodesShow, function (n, obj) {
+                    var pathOfOne = obj.getPath();
+                    if (pathOfOne && pathOfOne.length > 0) {
+                        for (var i = 0; i < pathOfOne.length - 1; i++) {
+                            tree.showNode(pathOfOne[i]);
+                            tree.expandNode(pathOfOne[i], true);
+                        }
+                    }
+                });
+            } else {
+                var rootNodes = tree.getNodesByParam("level", "0");
+                $.each(rootNodes, function (n, obj) {
+                    tree.expandNode(obj, true);
+                });
+            }
+        }
+    }
+
+    var lastKeyword = "";
+    $("#fuzzySearch").bind("input propertychange", function () {
+        if (!tree) return;
+        var _keywords = $(this).val();
+        if (lastKeyword === _keywords) {
+            return;
+        }
+        lastKeyword = _keywords;
+        searchNodeLazy(_keywords);
+    });
+
+    var timeoutId = null;
+
+    function searchNodeLazy(_keywords) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(function () {
+            ztreeFilter(tree, _keywords);
+        }, 500);
+    }
+})();
+
+(function gotoTop() {
+
+    $("#gotoTop").click(function () {
+        $('html,body').animate({scrollTop: '0px'}, 'normal');
+    });
+
+    function showGotoTop() {
+        var s = $(window).scrollTop();
+        if (s > 50) {
+            $("#gotoTop").fadeIn(500);
+        } else {
+            $("#gotoTop").fadeOut(500);
+        }
+        ;
+    }
+
+    $(window).scroll(showGotoTop);
+    $("#container").resize(showGotoTop);
+    $(window).resize(function () {
+        $("#gotoTop").offset({left: $("#middle").offset().left - 19.5});
+        showGotoTop();
+    });
+    $(window).resize();
+
+})();
+
+function zTreeOnRightClick(event, treeId, treeNode) {
+    if (treeNode.isParent) return;
+    $("#modalTitle").html(treeNode.getPath().map(a => a.originalKey).join(" > "));
+    $("#modalContent").html(treeNode.originalValue);
+    $("#detailModal").css('display', 'flex');
+};
+
+$(".modal-dialog ").click(function (e) {
+    e.stopPropagation();
+});
+$("#detailModal").click(function () {
+    $("#detailModal").hide();
+});
